70c60e7094219efadd9ca6741a175fc78cae5495
GeWei <grey@pku.edu.cn>
2023-03-15 16:41:37 +0800

    add single-thread-server

---
+++ single-thread-server/src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let (status_line, filename) = if http_request[0].starts_with("GET / HTTP/1.1") {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{status_line}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
